6b9bfa5ac746f01dc5e646a74976a3bceb400b77
David <bigway8@vk.com>
2022-10-17 04:43:08 +0300

    lesson 1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input type="number" v-model="num1" />
+    <input type="number" v-model="num2" />
+    <p>result: {{ result }}</p>
+
+    <div>
+      <button v-on:click="calculate">+</button>
+      <button v-on:click="calculate">-</button>
+      <button v-on:click="calculate">*</button>
+      <button v-on:click="calculate">/</button>
+      <button v-on:click="calculate">x^y</button>
+      <button v-on:click="calculate">x_y</button>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      num1: 0,
+      num2: 0,
+      result: 0,
+    }
+  },
+  methods: {
+    calculate(e) {
+      const action = e.target.textContent
+      const num1 = Number(this.num1)
+      const num2 = Number(this.num2)
+      switch (action) {
+        case '+':
+          this.result = num1 + num2
+          break
+        case '-':
+          this.result = num1 - num2
+          break
+        case '*':
+          this.result = num1 * num2
+          break
+        case '/':
+          this.result = num1 / num2
+          break
+        case 'x^y':
+          this.result = num1 ** num2
+          break
+        case 'x_y':
+          this.result = Math.floor(num1 / num2)
+          break
+      }
+    },
+  },
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="scss"></style>
